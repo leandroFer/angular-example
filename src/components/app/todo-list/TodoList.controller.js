3f07e44e2d056c16d635f8911533d4550ebd9225
Leandro Fernandez <leandro.fernandez@despegar.com>
2017-07-10 13:57:16 -0300

    delete carlos :|

--- src/components/app/todo-list/TodoList.controller.js
+++ src/components/app/todo-list/TodoList.controller.js
@@ -4,7 +4,7 @@ class TodoListController {
     this.campos = [{placeholder:'nombre',required:true},{placeholder:'apellido',required:true},{placeholder:'edad'}];
     this.columns = ['id','nombre','apellido','edad'];
     this.data = [{id:1,nombre:'Leandro',apellido:'Fernandez',edad:'22'},
-                  {id:2,nombre:'Ezequiel',apellido:'Cabrera',edad:'30'}]
+                  {id:2,nombre:'Ezequiel',apellido:'Cabrera',edad:'29'}]
     this.nextId = this.data.slice(-1)[0].id + 1;
   }
   
@@ -12,27 +12,23 @@ class TodoListController {
     //acciones relacionadas con los bindings que recibe el componente 
   }
   
-  handleAdd(elem) {
+  handleAdd({data}) {
     this.data.push({
       id : this.nextId,
-      nombre : elem.data.nombre,
-      apellido : elem.data.apellido,
-      edad : elem.data.edad
+      nombre : data.nombre,
+      apellido : data.apellido,
+      edad : data.edad
     })
-    this.nextId++
+    this.nextId++;
   } 
-  
-  elementInData(id) {
-    return this.data.find(element => element.id === id)
-  }
   
   deleteElem(index) {
     this.data.splice(index,1);
   }
   
-  handleDelete(element) {
-    let index = this.elementInData(element.id);
-    if(index){
+  handleDelete({id}) {
+    let index = this.data.map(elem => elem.id).indexOf(id)
+    if(index !== -1){
       this.deleteElem(index)
     }
   }
